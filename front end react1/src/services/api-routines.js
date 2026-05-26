a082ed0e005f4ddc8eb3421cb7be218e58875812
Jose Pedro <jeremitafilho@gmail.com>
2026-05-26 00:21:05 -0300

    tela de login responsiva

--- front end react1/src/services/api-routines.js
+++ front end react1/src/services/api-routines.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://fitness-app-produ-o.onrender.com',
-  withCredentials: true,
+const api = axios.create({ baseURL: 'https://fitness-app-produ-o.onrender.com' });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('auth_token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export const getRoutines = async () => {
